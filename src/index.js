5e67ad1d0b2b34bafa8c99c5ad6bf78bfae090d2
madhurachar2 <madhurachar2@gmail.com>
2022-08-03 20:46:03 +0530

    port

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const routes = require("./routers");
+const routes = require('./routers');
 
 //db connection
-const db = require("./database/db");
+const db = require('./database/db');
 db.authenticate()
-  .then(() => {
-    console.log("db connected");
-  })
-  .catch((error) => {
-    console.log("error" + error);
-  });
+	.then(() => {
+		console.log('db connected');
+	})
+	.catch((error) => {
+		console.log('error' + error);
+	});
 
-app.use(cors("*"));
+app.use(cors('*'));
 app.use(express.json());
-app.use("/", routes);
+app.use('/', routes);
 
-app.listen(4001, () => {
-  console.log("server started on port 4001");
+app.listen(3000, () => {
+	console.log('server started on port 4001');
 });
